03065206b7f0a301942d57d53689ded41bd140b6
Michal Harakal <michal.harakal@googlemail.com>
2025-06-23 20:35:43 +0200

    Fix Uuid in wasm

--- ka2a-client/src/wasmJsMain/kotlin/sk/ai/net/solutions/ka2a/client/util/UuidUtil.kt
+++ ka2a-client/src/wasmJsMain/kotlin/sk/ai/net/solutions/ka2a/client/util/UuidUtil.kt
@@ -1,20 +1,25 @@
 package sk.ai.net.solutions.ka2a.client.util
 
 import kotlin.random.Random
+import kotlin.time.TimeSource
 
 /**
  * WebAssembly JS implementation of UuidUtil.
  */
 actual object UuidUtil {
+    // Use a monotonic time source to get a timestamp
+    private val timeSource = TimeSource.Monotonic
+    private val startMark = timeSource.markNow()
+
     /**
      * Generates a random UUID string for WebAssembly JS.
      *
      * @return A random UUID string.
      */
     actual fun randomUuid(): String {
-        // Simple implementation that generates a UUID-like string using pure Kotlin
-        val timestamp = "currentTimeMillis().toString()"
+        // Simple implementation that generates a UUID-like string using elapsed time and random number
+        val elapsedMillis = startMark.elapsedNow().inWholeMilliseconds
         val random = Random.nextInt(0, 1000000).toString()
-        return "wasm-$timestamp-$random"
+        return "wasm-$elapsedMillis-$random"
     }
 }
